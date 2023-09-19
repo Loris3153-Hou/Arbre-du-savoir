c961a406d93c54212b9d237b038e0cef1cb7e413
loris <lorishourriere@gmail.com>
2023-09-19 17:17:02 -0400

    correction menu

--- docs/vues/accueil.php
+++ docs/vues/accueil.php
@@ -10,26 +10,27 @@
 
     <body>
     <header>
+        <div class="hamburger-menu">
+            <input id="menu__toggle" type="checkbox" />
+            <label class="menu__btn" for="menu__toggle">
+                <span></span>
+            </label>
+
+            <ul class="menu__box">
+                <li><a class="menu__item" href="#">Home</a></li>
+                <li><a class="menu__item" href="#">About</a></li>
+                <li><a class="menu__item" href="#">Team</a></li>
+                <li><a class="menu__item" href="#">Contact</a></li>
+                <li><a class="menu__item" href="#">Twitter</a></li>
+            </ul>
+        </div>
         <!--<img src='../images/hamb.png' alt='Programmer en C' width='90px' height='80px'>-->
         <h1>Arbre du Savoir</h1>
         <img src='../images/logo.png' alt='Programmer en C' width='90px' height='80px'>
     </header>
 
     <!-- **************************************************************************************************** -->
-    <div class="hamburger-menu">
-        <input id="menu__toggle" type="checkbox" />
-        <label class="menu__btn" for="menu__toggle">
-            <span></span>
-        </label>
 
-        <ul class="menu__box">
-            <li><a class="menu__item" href="#">Home</a></li>
-            <li><a class="menu__item" href="#">About</a></li>
-            <li><a class="menu__item" href="#">Team</a></li>
-            <li><a class="menu__item" href="#">Contact</a></li>
-            <li><a class="menu__item" href="#">Twitter</a></li>
-        </ul>
-    </div>
 
     <!-- **************************************************************************************************** -->
 
