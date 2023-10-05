a76304637329100f5b41936959073ff5f4f19dd5
Nolan10 <nolan.tessier.etu@univ-lemans.fr>
2023-10-04 21:19:44 -0400

    Création des deux fichiers pour le page panier #36

---
+++ docs/vues/panier.php
@@ -0,0 +1,21 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang='fr'>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Arbre du Savoir</title>
+        <link href="css/panier.css" rel="stylesheet">
+        <link href="css/menu.css" rel="stylesheet">
+        <script src="js/modifier-produit.js"></script>
+    </head>
+    <body>
+        <?php include("header.php")?>
+
+        <footer>
+            <h3>Condition d'utilisation</h3>
+            <h3>Date mise à jour</h3>
+            <h3>Réseaux sociaux</h3>
+        </footer>
+    </body>
+</html>
